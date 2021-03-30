d0ce64c39c9603c41fd036fd39baaf82949ba537
Deepseekz <lheritier.adam@gmail.com>
2021-03-30 09:42:25 +0200

    RecyclerView fonctionnel

--- app/src/main/java/Activities/MainActivity.java
+++ app/src/main/java/Activities/MainActivity.java
@@ -1,33 +1,48 @@
 package Activities;
 
+import Adapters.RecyclerViewAdapter;
 import Models.GsonRequest;
 import Models.Visiteur;
 import Models.Visiteurs;
 import Models.VolleyHelper;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.example.Activities.R;
+import com.example.Activities.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    final private String visiteursUrl = "http://192.168.210.4/cakephp/index.php/visiteurs.json";
-
-    private TextView textViewTest;
+    private ActivityMainBinding binding;
+    private String visiteursUrl = "http://192.168.210.4/cakephp/visiteurs.json";
+    private RecyclerViewAdapter adapter;
+    private ArrayList<Visiteur> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
 
-        setContentView(R.layout.activity_main);
-
-        textViewTest = findViewById(R.id.test);
-
+        setContentView(view);
+        initializeUI();
         openVisitesDatas();
+    }
+
+    private void initializeUI() {
+        binding.rvPraticiens.setHasFixedSize(true);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        binding.rvPraticiens.setLayoutManager(layoutManager);
+        binding.rvPraticiens.setFocusable(false);
 
     }
 
@@ -36,12 +51,8 @@ public class MainActivity extends AppCompatActivity {
         final GsonRequest gsonRequest = new GsonRequest(visiteursUrl, Visiteurs.class, null, new Response.Listener<Visiteurs>() {
             @Override
             public void onResponse(Visiteurs visiteurs) {
-                String resultat="";
-                for (Visiteur prev : visiteurs.getVisiteurs()) {
-                    resultat+=prev.toString();
-                }
-
-                textViewTest.setText(resultat);
+                adapter = new RecyclerViewAdapter(visiteurs.getVisiteurs());
+                binding.rvPraticiens.setAdapter(adapter);
             }
         },
                 new Response.ErrorListener() {
